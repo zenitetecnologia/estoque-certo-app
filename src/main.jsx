b6057ec64e8f6690a41568da452fddce194f75a4
Joaofernaandess <jf030626@gmail.com>
2026-06-12 16:26:28 -0300

    issue #106

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { registerSW } from 'virtual:pwa-register';
 import App from './App.jsx';
+import './styles/custom.css';
 
 registerSW({
   onNeedRefresh() {
